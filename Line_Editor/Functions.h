61875e45e1c1df59511f1b9036a0a6bbd6720b49
stephen <sfetinko2gmail.com>
2018-01-15 11:50:21 -0400

    Got the program checking the current file path for the program

---
+++ Line_Editor/Functions.h
@@ -0,0 +1,16 @@
+//
+// Created by stephen on 10/01/18.
+//
+
+#ifndef LINE_EDITOR_FUNCTIONS_H
+#define LINE_EDITOR_FUNCTIONS_H
+#include <iostream>
+
+class Functions {
+public:
+    // Public functions
+    void helpScreen();
+};
+
+
+#endif //LINE_EDITOR_FUNCTIONS_H
